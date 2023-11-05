3be26197bb22001aafeef80d5f2c6c54efed6080
ThanhLuan <ntluan51@gmail.com>
2023-11-05 20:10:11 +0700

    AddLayout

--- QL_KhoaHocOnl/Models/Courses.Designer.cs
+++ QL_KhoaHocOnl/Models/Courses.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\ADMIN\source\repos\QL_KhoaHocOnl\QL_KhoaHocOnl\Models\Courses.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Document\Hoc&Code\HocTap\HK5_Nam3\LTW\DoAn\QL_KhoaHocOnl\QL_KhoaHocOnl\Models\Courses.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
