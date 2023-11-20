1115367cc6c35aa6b3b21efef74e6f532cd0663d
ThanhLuan <ntluan51@gmail.com>
2023-11-20 20:17:08 +0700

    AddSearchSort

--- QL_KhoaHocOnl/Controllers/CourseController.cs
+++ QL_KhoaHocOnl/Controllers/CourseController.cs
@@ -11,15 +11,48 @@ namespace QL_KhoaHocOnl.Controllers
     {
         // GET: Course
         QL_COURSEEntities db = new QL_COURSEEntities();
-        public ActionResult Index()
+        public ActionResult Index(string search = "", string sort = "", int page = 1)
         {
-            List<COURSE> list = db.COURSE.ToList();
+            List<COURSE> list = db.COURSE.Where(x=>x.NAME_COURSE.Contains(search)).ToList();
+            ViewBag.TB = search;
+
+            //Sort
+            ViewBag.Sort = sort;    
+            if (sort == "Up")
+            {
+                list = list.OrderBy(x => x.PRICE_COURSE).ToList();
+            }
+            else
+            {
+                list = list.OrderByDescending(x => x.PRICE_COURSE).ToList();
+            }
+
+            // Paging 
+            int NoOfRecordPerPage = 9;
+            int NoOfPages = Convert.ToInt32(Math.Ceiling(Convert.ToDouble(list.Count) / Convert.ToDouble(NoOfRecordPerPage)));
+            int NoOfRecordSkip = (page - 1) * NoOfRecordPerPage;
+            ViewBag.Page = page;
+            ViewBag.NoOfPages = NoOfPages;
+            list = list.Skip(NoOfRecordSkip).Take(NoOfRecordPerPage).ToList();
             return View(list);
         }
         public ActionResult Detail(string id)
         {
             COURSE course = db.COURSE.Where(c => c.ID_COURSE == id).FirstOrDefault();
+            Session["ID"] = id;
             return View(course);
+        }
+        public ActionResult Lesson(string id)
+        {
+            List<LESSON> lst = db.LESSON.Where(c => c.ID_COURSE == id).ToList();
+            int i = lst.Count;
+            ViewBag.Count = i;
+            return View(lst);
+        }
+        public ActionResult DetailLesson(string id)
+        {
+            LESSON ls = db.LESSON.Where(c => c.ID_LESSON == id).FirstOrDefault();
+            return View(ls);
         }
     }
 }
