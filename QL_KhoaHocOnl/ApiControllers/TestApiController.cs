e7835772eac7ea386c98505a1b2c96503eaf11f2
ThanhLuan <ntluan51@gmail.com>
2023-11-26 20:13:08 +0700

    Add Api

---
+++ QL_KhoaHocOnl/ApiControllers/TestApiController.cs
@@ -0,0 +1,24 @@
+﻿using QL_KhoaHocOnl.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Data.Entity;
+using System.Web.Http;
+
+namespace QL_KhoaHocOnl.ApiControllers
+{
+    public class TestApiController : ApiController
+    {
+
+        QL_COURSEEntities db = new QL_COURSEEntities();
+        public List<COURSE> Get()
+        {
+            return db.COURSE.ToList();
+        }
+        public COURSE Get(string id)
+        {
+            return db.COURSE.Where(x => x.ID_COURSE == id).FirstOrDefault();
+        }
+    }
+}
--- QL_KhoaHocOnl/Global.asax.cs
+++ QL_KhoaHocOnl/Global.asax.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -12,7 +13,11 @@ namespace QL_KhoaHocOnl
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
         }
+
     }
 }
